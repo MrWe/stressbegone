47cc166c8b4881228cba35b29ff81c64625f10bd
Erik Wiker <Wiker@dhcp-039175.wlan.ntnu.no>
2014-12-02 15:29:38 +0100

    Better code, checks if image is broken and tries to find another

--- popup.js
+++ popup.js
@@ -1,32 +1,39 @@
-var respArray = [];
-
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "http://www.reddit.com/r/eyebleach/.json", true);
-xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4) {
-        // JSON.parse does not evaluate the attacker's scripts.
-        var resp = JSON.parse(xhr.responseText);
-        respArray = resp.data.children;
-
-        for (var i = 0; i < respArray.length; i++) {
-            try {
-                console.log(respArray[i].data.url);
-                var img = document.createElement("img");
-                img.src = respArray[i].data.url;
-
-                var ul = document.getElementById("imgList");
-                var li = document.createElement("li");
-                li.appendChild(img);
-                ul.appendChild(li);
-
-
-            } catch (err) {
-                console.log(err);
-            }
+window.addEventListener('load', function(e) {
+    var respArray = [];
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://www.reddit.com/r/eyebleach/.json", true);
+    var img = document.getElementById("image");
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            // JSON.parse does not evaluate the attacker's scripts.
+            var resp = JSON.parse(xhr.responseText);
+            respArray = resp.data.children;
+            getRandUrl(respArray);
         };
     }
-}
-xhr.send();
 
-var test = document.getElementById("p");
-test.innerHTML = "Hei";
+    xhr.send();
+
+    function getRandUrl(array) {
+        try {
+            var randURL = array[Math.floor((Math.random() * array.length) + 1)].data.url;
+            console.log(randURL + "");
+        } catch (err) {
+            console.log(err);
+        }
+        if (checkImgUrl(randURL) === true) {
+        }
+    }
+
+    function checkImgUrl(url) {
+        img.src = url;
+        image.onerror = function() {
+            getRandUrl(respArray);
+            console.log("fuck");
+            return false;
+        };
+        return true;
+    }
+
+
+});
